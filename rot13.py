17862381e1dad81aef782b6e3403c7863c5f3f39
sebastian00000001 <sebastian.nawrot@outlook.com>
2018-07-16 13:26:30 +0200

    Add rot(text, key) function.

--- rot13.py
+++ rot13.py
@@ -2,12 +2,14 @@ import sys
 import string
 
 alphabet = string.ascii_lowercase # a...z
-key = "nopqrstuvwxyzabcdefghijklm" # rot13
+
 
 def rot13(text):
     "This prints a text after rot13 (lowercase)"
+    key = "nopqrstuvwxyzabcdefghijklm" # rot13
+    
     text = text.lower()
-    print("[output]: ", end = "")
+    print("[i] rot13: ", end = "")
     for i in text:
         position = str.find(alphabet, i)
         if position != -1:
@@ -15,13 +17,31 @@ def rot13(text):
         else:
             print(i, end = "")
 
+def rot(text, key):
+    "This print a text after rot<KEY> (lowercase)"
+    alphabetKey = alphabet[key:]
+    alphabetKey += alphabet[:key]
+    text = text.lower()
+    print("[i] rot{}: ".format(key), end = "")
+    for i in text:
+        position = str.find(alphabet, i)
+        if position != -1:
+            print(alphabetKey[position], end = "")
+        else:
+            print(i, end = "")
+
 if len(sys.argv) < 2:
     print("[!] Too few arguments.")
     print("[i] Usage: {} \"text to encrypt/decrypt\"".format(sys.argv[0]))
+elif len(sys.argv) == 3:
+    plaintext = sys.argv[1]
+    print("[input]: {}".format(plaintext))
+    rot(plaintext, int(sys.argv[2]))
 else:
     plaintext = sys.argv[1]
     print("[input]: {}".format(plaintext))
     rot13(plaintext)
+    
 
 
     
