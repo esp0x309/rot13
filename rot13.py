8f4837f8a53552de9627e845b8dfa03af3d6c97e
seastian00000001 <sebastian.nawrot@outlook.com>
2018-07-16 12:01:50 +0200

    Add rot13 function

--- rot13.py
+++ rot13.py
@@ -1,18 +1,28 @@
+import sys
 import string
 
 alphabet = string.ascii_lowercase # a...z
 key = "nopqrstuvwxyzabcdefghijklm" # rot13
 
-plaintext = "sEbaStIan12 sepo"
-plaintext = plaintext.lower()
+def rot13(text):
+    "This prints a text after rot13 (lowercase)"
+    text = text.lower()
+    print("[output]: ", end = "")
+    for i in text:
+        position = str.find(alphabet, i)
+        if position != -1:
+            print(key[position], end = "")
+        else:
+            print(i, end = "")
 
-print(plaintext)
+if len(sys.argv) < 2:
+    print("[!] Too few arguments.")
+    print("[i] Usage: {} \"text to encrypt/decrypt\"".format(sys.argv[0]))
+else:
+    plaintext = sys.argv[1]
+    print("[input]: {}".format(plaintext))
+    rot13(plaintext)
 
-for i in plaintext:
-    position = str.find(alphabet, i)
-    if position != -1:
-        print(key[position], end = "")
-    else:
-         print(i, end = "")
+
     
 
